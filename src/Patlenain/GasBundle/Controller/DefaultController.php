ed9153eb4b887c3619bb03f8baca7371cbece53c
patlenain <patrice@legurun.org>
2013-10-26 10:38:37 +0200

    Force l'authentification pour la page d'accueil

--- src/Patlenain/GasBundle/Controller/DefaultController.php
+++ src/Patlenain/GasBundle/Controller/DefaultController.php
@@ -5,12 +5,14 @@ namespace Patlenain\GasBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="patlenain_gas_index")
      * @Template()
+     * @Secure(roles="ROLE_USER")
      */
     public function indexAction()
     {
