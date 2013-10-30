8f307df8551a89c9476cab45bce15ed3a3ea23d9
patlenain <patrice@legurun.org>
2013-10-30 04:23:11 +0100

    Supprime un import inutile

--- src/Patlenain/GasBundle/Controller/DefaultController.php
+++ src/Patlenain/GasBundle/Controller/DefaultController.php
@@ -4,7 +4,6 @@ namespace Patlenain\GasBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class DefaultController extends Controller
