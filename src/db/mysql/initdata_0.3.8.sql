e64cb9c289b824281d1760394219ef754ae742e1
patlenain <patrice@legurun.org>
2014-11-13 04:08:58 +0100

    Préparation version 0.3.8

---
+++ src/db/mysql/initdata_0.3.8.sql
@@ -0,0 +1,2 @@
+INSERT INTO utilisateur (username, nom, email, salt, password, admin) VALUES ('admin','Administrateur','root@localhost','f14a8a2a4ce338ca9179981327ce5aff','64e871a56f55330de03b624ea8edae5caef314518d8c5f6a8684adecf2ace5923de926bd85109ee345c77b58ac07bc0754d5c1db31684cdd94e8bfa338ede9f6',1);
+
